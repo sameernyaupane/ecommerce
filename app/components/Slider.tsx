ce6ff1ba3fdaa47e5dfb0cb0c6fb1f382938a486
Sameer Nyaupane <sameernyaupane@outlook.com>
2024-11-04 19:48:22 +0545

    Fix slider

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -9,33 +8,31 @@ import {
 
 export function Slider() {
   return (
-    <Carousel
-    className='mt-[2rem]'
-  >
-    <CarouselContent>
-      <CarouselItem
-        key="1"
-        className='flex justify-center items-center'
-      >
-        <div className='p-1 w-[1200px]'>
-          <Card className='h-[400px]'>
-            <CardContent className='flex items-center justify-center p-6'>
-              <div className="absolute top-10 left-32 z-10 p-6 text-white">
+    <Carousel className='my-[2rem]'>
+      <CarouselContent>
+        <CarouselItem
+          key="1"
+          className='flex justify-center items-center'
+        >
+          <div className='w-[1200px]'>
+            <div className='h-[400px] relative'>
+              <div className="absolute top-10 left-[50px] z-10 p-6 text-white">
                 <h2 className='text-5xl font-semibold mb-2'>Transform Your Look</h2>
                 <p className='text-xl font-medium'>Explore <strong>INDIBE Marketplace</strong> for <br /> Exclusive Beauty Discoveries!</p>
               </div>
-              <img 
-                src="/images/carousel/backgroundsliderimage.png" 
-                className="w-full h-full object-cover scale-200 transform origin-center"
-              />
-            </CardContent>
-          </Card>
-        </div>
-      </CarouselItem>
-    </CarouselContent>
-    <CarouselPrevious className='absolute left-[2rem] top-1/2 transform -translate-y-1/2 z-10' />
-    <CarouselNext className='absolute right-[2rem] top-1/2 transform -translate-y-1/2 z-10' />
-  </Carousel>
+              <div className='w-full h-full relative'>
+                <img 
+                  src="/images/carousel/backgroundsliderimage.png" 
+                  className="w-full h-full object-cover scale-200 transform origin-center"
+                />
+                <CarouselPrevious className='absolute left-4 top-1/2 -translate-y-1/2 z-20' />
+                <CarouselNext className='absolute right-4 top-1/2 -translate-y-1/2 z-20' />
+              </div>
+            </div>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
   );
 }
 
